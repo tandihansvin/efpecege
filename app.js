fbb6df1a9821d1d672fdde0186a700de47e1b15c
〔≡◉︵◉≡〕 <anakaiti@users.noreply.github.com>
2018-06-06 19:33:30 +0700

    Never be alone again

--- app.js
+++ app.js
@@ -1,26 +1,66 @@
-// Level offset
-const offsetX = 30;
-const offsetY = 7; 
-
-// Mesh globals
-let player;
-let small;
-let platform;
-let listOfBox = [];
-
-let loaded = false; // Flag to check if json is loaded
-
-let direction = 1;
-
-let state;
+// Data store
 let jsonData;
-let level = "2";
+const offsetX = 30;
+const offsetY = 7;
 
-function createMat(scene,pic){
-    let mat = new BABYLON.StandardMaterial("materialTex", scene);
-    mat.diffuseTexture = new BABYLON.Texture(pic, scene);
-    mat.alpha = 1;
-    return mat;
+// Session store
+let level = 1;
+let state = 0; // 0: loading; 1: playing; 2: lose; 3: win;
+
+// Meshes
+let player, platform;
+let rightSmall, leftSmall;
+let listOfBox = [];
+let exit;
+
+// Scene globals
+let camera;
+
+//controller
+let up, dn, lf, rg;
+let direction;
+
+function createBackground(scene){
+    let mat = new BABYLON.StandardMaterial("Mat", scene);
+    mat.alpha = 0;
+
+    let plane = BABYLON.MeshBuilder.CreatePlane("plane", {height: 18, width: 60}, scene);
+    plane.material = mat;
+    plane.setPhysicsState(BABYLON.PhysicsEngine.PlaneImpostor, { mass: 0, restitution: 0.3, friction: 0, move: false });
+
+    let inviPlane = BABYLON.MeshBuilder.CreatePlane("plane2", {height: 18, width: 60}, scene);
+    inviPlane.position.z = -1;
+    inviPlane.material = mat;
+    inviPlane.setPhysicsState(BABYLON.PhysicsEngine.PlaneImpostor, { mass: 0, restitution: 0.3, friction: 0, move: false });
+}
+
+function createSkybox(scene){
+    let skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:250.0}, scene);
+    let skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/skybox/greenhaze", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
+}
+
+function createScene(engine){
+    //creating scene
+    let scene = new BABYLON.Scene(engine);
+    scene.enablePhysics(new BABYLON.Vector3(0, 0, 0), new BABYLON.OimoJSPlugin());
+
+    // Camera
+    camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 4, new BABYLON.Vector3(-6, 0, 0), scene);
+    camera.setPosition(new BABYLON.Vector3(-16,3,-16));
+
+    // Light
+    let light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(1, 0, -1), scene);
+
+    //background
+    createBackground(scene);
+    createSkybox(scene);
+    return scene;
 }
 
 function createPlayer(start, scene) {
@@ -39,17 +79,13 @@ function createPlayer(start, scene) {
 }
 
 function createExit(end, scene) {
-    let exit = new BABYLON.Mesh.CreateBox("exit", 1, scene);
+    exit = new BABYLON.Mesh.CreateBox("exit", 1, scene);
     exit.position = new BABYLON.Vector3(end[0]-offsetX, offsetY-end[1], -0.5);
     exit.physicsImpostor = new BABYLON.PhysicsImpostor(exit, BABYLON.PhysicsImpostor.BoxImpostor, {mass:0, restitution:0, friction:0},scene);
     exit.physicsImpostor.registerOnPhysicsCollide(player.physicsImpostor, function(main, collided){
-        console.log('you win !!');
-    });
+        handleWin(scene);
+    });;
 }
-
-// function turnBackBox(main, collided){
-//     console.log("TURN BACK BOX");
-// }
 
 function createPlatform(box, boxSize, scene){
     platform = new BABYLON.Mesh.CreateBox("platform", 1, scene);
@@ -57,7 +93,6 @@ function createPlatform(box, boxSize, scene){
     platform.position = new BABYLON.Vector3(box[0]-offsetX, offsetY-box[1], -0.5);
     platform.physicsImpostor = new BABYLON.PhysicsImpostor(platform, BABYLON.PhysicsImpostor.BoxImpostor, {mass:20, restitution:0, friction:0},scene);
     // platform.physicsImpostor.registerOnPhysicsCollide(small.physicsImpostor, turnBackBox);
-    listOfBox.push(platform);
 }
 
 function createBox(scene,x,y,z){
@@ -68,84 +103,115 @@ function createBox(scene,x,y,z){
     listOfBox.push(box);
 }
 
-function createBackground(scene){
-    let plane = BABYLON.MeshBuilder.CreatePlane("plane", {height: 18, width: 60}, scene);
-    let planeMat = createMat(scene, 'http://localhost:8000/tanah.jpg');
-    plane.material = planeMat;
-    plane.setPhysicsState(BABYLON.PhysicsEngine.PlaneImpostor, { mass: 0, restitution: 0.3, friction: 0, move: false });
-
-    //invisible plane
-    let mat = new BABYLON.StandardMaterial("Mat", scene);
-    mat.alpha = 0;
-    let inviPlane = BABYLON.MeshBuilder.CreatePlane("plane2", {height: 18, width: 60}, scene);
-    inviPlane.position.z = -1;
-    inviPlane.material = mat;
-    inviPlane.setPhysicsState(BABYLON.PhysicsEngine.PlaneImpostor, { mass: 0, restitution: 0.3, friction: 0, move: false }); 
-}
-
-function checkCollision(){
-    for(let i=0;i<listOfBox.length;i++){
-        if(leftSmall.intersectsMesh(listOfBox[i],true) && direction == -1){
-            direction *= -1;
-            break;
-        }
-        else if(rightSmall.intersectsMesh(listOfBox[i],true) && direction == 1){
-            direction *= -1;
-            break;
+function createMeshes(scene, lvl) {
+    createPlayer(jsonData[lvl]["start"], scene);
+    createExit(jsonData[lvl]["end"], scene);
+    createPlatform(jsonData[lvl]["box"], jsonData[lvl]["boxSize"], scene);
+    // Generate block
+    for(let i=0;i<jsonData[lvl]["maze"].length;i++){
+        for(let j=0;j<jsonData[lvl]["maze"][i].length;j++){
+            if (jsonData[lvl]["maze"][i][j] == 'x') {
+                createBox(scene, j-offsetX, offsetY-i,-0.5);
+            }
         }
     }
 }
 
-function resetPosition(scene){
+function destroyMeshes() {
+    // Dispose objects
+    if (player) player.dispose();
+    if (platform) platform.dispose();
+    if (exit) exit.dispose();
+    for(let i=0;i<listOfBox.length;i++){
+        listOfBox[i].dispose();
+    }
+
+    // Clear vars
+    player = undefined;
+    platform = undefined;
+    exit = undefined;
+    listOfBox = [];
+}
+
+function renderLevel(scene, lvl){
+    state = 0;
+    destroyMeshes();
+    createMeshes(scene, lvl);
+    direction = 1;
+    state = 1;
+}
+
+function platformMovement(){
+    const PLATFORM_SPEED = 8;
+
+    let impostor = platform.physicsImpostor;
+
+    let xav = 0, yav = 0;
+
+    if (lf) xav = -PLATFORM_SPEED;
+    else if (rg) xav =  PLATFORM_SPEED;
+
+    if (up) yav =  PLATFORM_SPEED;
+    else if (dn) yav = -PLATFORM_SPEED;
+
+    impostor.setLinearVelocity(new BABYLON.Vector3(xav, yav, 0));
+    impostor.setAngularVelocity(new BABYLON.Quaternion(0,0,0,0));
+
+    camera.setTarget(new BABYLON.Vector3(platform.position.x,0,0));
+    camera.setPosition(new BABYLON.Vector3(platform.position.x,3,-16));
+}
+
+function checkCollision(){
+    function chk(item) {
+        if(leftSmall.intersectsMesh(item,true) && direction == -1){
+            direction *= -1;
+        }
+        else if(rightSmall.intersectsMesh(item,true) && direction == 1){
+            direction *= -1;
+        }
+    }
+
+
+    for(let i=0;i<listOfBox.length;i++){
+        chk(listOfBox[i]);
+    }
+    chk(platform);
+}
+
+function playerMovement(){
+    const PLAYER_SPEED = 1;
+    checkCollision();
+    let plv = player.physicsImpostor.getLinearVelocity();
+
+    player.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(PLAYER_SPEED * direction, -4, 0));
+
+    if(player.position.y < -offsetY){
+        state = 2;
+        handleLose();
+    }
+}
+
+function handleLose(){
+    // Reset positions
     let start = jsonData[level]["start"];
     player.position = new BABYLON.Vector3(start[0]-offsetX, offsetY-start[1], -0.5);
 
     let box = jsonData[level]["box"];
     platform.position = new BABYLON.Vector3(box[0]-offsetX, offsetY-box[1], -0.5);
-    
+
     direction = 1;
-    loaded = true;
-    state = "playing";
+    state = 1;
 }
 
-window.addEventListener('DOMContentLoaded', function(){
-    let canvas = document.getElementById('renderCanvas');
-    state = "playing";
+function handleWin(scene){
+    if (state === 1){
+        state=3;
+        // renderLevel(scene, level + 1);
+        // level += 1;
+    }
+}
 
-    let engine = new BABYLON.Engine(canvas, true);
-
-    //creating scene
-    let scene = new BABYLON.Scene(engine);
-    scene.enablePhysics(new BABYLON.Vector3(0, 0, 0), new BABYLON.OimoJSPlugin());
-
-    let camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 4, new BABYLON.Vector3(-16, 0, 0), scene);
-    // camera.attachControl(canvas, true);
-    camera.setPosition(new BABYLON.Vector3(-16,3,-16));
-    let light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(1, 0, -1), scene);
-    
-    //background
-    createBackground(scene);
-
-    //generate maze
-    let url = "http://localhost:8000/level.json";
-    let message = $.get(url);
-    message.done(function(data){
-        jsonData=data;
-        createPlayer(jsonData[level]["start"], scene);
-        createExit(jsonData[level]["end"], scene);
-        createPlatform(jsonData[level]["box"], jsonData[level]["boxSize"], scene);
-        // Generate block
-        for(let i=0;i<jsonData[level]["maze"].length;i++){
-            for(let j=0;j<jsonData[level]["maze"][i].length;j++){
-                if (jsonData[level]["maze"][i][j] == 'x') {
-                    createBox(scene, j-offsetX, offsetY-i,-0.5);
-                } 
-            }
-        }
-        resetPosition(scene);
-    });
-
-    let lf,rg,up,dn;
+function bindInput() {
     window.onkeydown = function(event){
         let code = event.keyCode;
         if(code==37){     //left
@@ -176,49 +242,42 @@ window.addEventListener('DOMContentLoaded', function(){
             dn=0;
         }
     };
+}
 
-    function platformMovement(){
-        const PLATFORM_SPEED = 8;
+function bindLevelSelector(scene) {
+    $("a").click(function() {
+        level = $(this).html();
+        renderLevel(scene, level);
+    });
+}
 
-        let impostor = platform.physicsImpostor;
+function loadJSON(scene){
+    let url = "http://localhost:8000/level.json";
+    let message = $.get(url);
+    message.done(function(data){
+        jsonData=data;
+        renderLevel(scene,level);
+    });
+}
 
-        let xav = 0, yav = 0;
+window.addEventListener('DOMContentLoaded', function(){
+    let canvas = document.getElementById('renderCanvas');
+    let engine = new BABYLON.Engine(canvas, true);
+    let scene = createScene(engine);
 
-             if (lf) xav = -PLATFORM_SPEED; 
-        else if (rg) xav =  PLATFORM_SPEED;
-        
-             if (up) yav =  PLATFORM_SPEED;
-        else if (dn) yav = -PLATFORM_SPEED;
-
-        impostor.setLinearVelocity(new BABYLON.Vector3(xav, yav, 0));
-        impostor.setAngularVelocity(new BABYLON.Quaternion(0,0,0,0));
-
-        camera.setTarget(new BABYLON.Vector3(platform.position.x,0,0));
-        camera.setPosition(new BABYLON.Vector3(platform.position.x,3,-16));
-    }
-
-    // Add gravity to player box
-    function playerMovement(){
-        const PLAYER_SPEED = 1;
-        checkCollision();
-        let plv = player.physicsImpostor.getLinearVelocity();
-        
-        player.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(PLAYER_SPEED * direction, -4, 0));
-
-        if(player.position.y < -offsetY){
-            state = "falling";
-        }
-    }
+    loadJSON(scene);
+    bindInput();
+    bindLevelSelector(scene);
 
     engine.runRenderLoop(function(){
-        scene.render();
-        if(loaded){
+        if (state === 1){
+            scene.render();
             platformMovement();
             playerMovement();
-            if(state=="falling") {
-                loaded=false;
-                resetPosition(scene);
-            }
         }
+    });
+
+    window.addEventListener('resize', function() {
+        engine.resize();
     });
 });
